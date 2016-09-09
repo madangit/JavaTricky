3a6b2ae639ae3bc302d8cb68dd6a2a57c32168f7
madangit <madan.mca@gmail.com>
2016-09-09 10:02:08 +0530

    Update MatrixTraverse.java

--- TechTalk/src/MatrixTraverse.java
+++ TechTalk/src/MatrixTraverse.java
@@ -1,7 +1,20 @@
 
-public class MatrixTraverse {
+	//Question: suppose you have a 5 * 5 matrix 
+	//we need to print the output as a zigzag read of the matrix
+	//meaning if the matrix is 
+	//  1  2  3  4  5
+	//  6  7  8  9 10
+	// 11 12 13 14 15
+	// 16 17 18 19 20
+	// 21 22 23 24 25
+	//
+	// The output should be 1 2 3 4 5 10 15 14 13 12 11 16 21 22 23 24 25 
 
+
+public class MatrixTraverse {
+	
 	public static void main(String[] args) {
+		
 		boolean forward = false;
 		boolean skipline = false;
 		int colcount = 5;
@@ -9,16 +22,15 @@ public class MatrixTraverse {
 		int filler = 0;
 		int temp[]= new int[colcount];
 		
+				
 		for (int currow=0; currow < rowcount; currow++) {
 			
-			if ( (currow & 1) == 0 ) {
-				if (forward == true) {
+				if ((forward == true) && (skipline ==false)) {
 					forward = false;
-				} else {
+				} else if ((forward == false) && (skipline ==false)) {
 					forward = true;
 				}
-			} 
-			
+
 			for (int curcol=0; curcol < colcount; curcol++) {
 				filler ++;
 				if (skipline == false) {
@@ -28,11 +40,10 @@ public class MatrixTraverse {
 						temp[curcol] = filler;
 					}
 				}
-				if ((skipline == true) && (forward == false) && (curcol == colcount -1) ) {
+				if ((skipline == true) && (forward == true) && (curcol == colcount -1) ) {
 					System.out.println(filler+"");
 				} else if ((skipline == true) && (forward == false) && (curcol == 0) ) {
 					System.out.println(filler+"");
-					forward = true;
 				}
 			}
 
